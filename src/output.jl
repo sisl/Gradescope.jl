87bd88aaf03d55b86e2582fc39cde9b87e81c9e1
Robert Moss <rmoss92@gmail.com>
2024-12-09 00:00:40 -0800

    Fixed typo in `output` doc string

--- src/output.jl
+++ src/output.jl
@@ -12,7 +12,7 @@ metadata(path="/autograder/submission_metadata.json") = JSON.parsefile(path)
     output(results::Results)
     output(results::Dict)
 
-Write Gradescope ouput formated JSON file to `results.json`
+Write Gradescope output formatted JSON file to `results.json`
 See: https://gradescope-autograders.readthedocs.io/en/latest/specs/#output-format
 """
 output(results::Union{Results, Dict}) = write("/autograder/results/results.json", json(results, 4))
